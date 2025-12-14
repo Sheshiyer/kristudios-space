8c5d498018598c5ef5cf4655369dce4ec923aad8
sheshiyer <sheshnarayan.iyer@gmail.com>
2025-12-14 10:48:58 +0530

    feat: Update website content with new Kristudios-branded images and enhanced SEO metadata.

---
+++ rename_images_batch2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /Users/sheshnarayaniyer/kristudios/website/OnScrollPathAnimations/src/img
+
+# Renaming the NEW file
+mv "thewhychromosome_Multi-tiered_traditional_Navratri_Gombe_Habb_0a9b49f3-f5fd-4141-a98b-86205e77d0c0_2.png" "kristudios-gombe-habba-navratri-tier.png"
+
+# Renaming the others (which seem to be re-added duplicates or missed files, ensuring they map to the same SEO names as before)
+mv "thewhychromosome_A_cinematic_extreme_macro_photograph_of_a_fi_aa528410-5b87-4af7-8f86-5386847c8d11_0.png" "kristudios-classical-dance-macro.png"
+mv "thewhychromosome_A_documentary-style_macro_photograph_of_a_pa_8f9c55c9-f3fd-4dde-948a-c5211798bbed_2.png" "kristudios-palm-leaf-manuscript-macro.png"
+mv "thewhychromosome_A_dramatic_close-up_photograph_of_an_artisan_88db95d7-fc78-4cb7-90a5-107de6e8298d_0.png" "kristudios-artisan-portrait-dramatic.png"
+mv "thewhychromosome_A_wide-angle_landscape_photograph_of_a_dimly_b7d3a950-4c34-46c6-bd8d-98a168636300_3.png" "kristudios-dim-temple-landscape.png"
+mv "thewhychromosome_An_intimate_close-up_photograph_of_aged_hand_d70919c0-b533-40f8-a360-7dd3426dbeaf_3.png" "kristudios-aged-hand-crafting-detail.png"
